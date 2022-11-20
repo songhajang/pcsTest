db5c033f8eef88b8001aa5592934be1a41aef64d
koldin108902 <alex108902@naver.com>
2022-11-21 02:53:56 +0900

    add: sign up

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import { Route, Routes } from "react-router-dom";
 import Home from "./component/home";
 import Login from "./component/login";
+import SignUp from "./component/signUp";
 
 function App() {
   return (
@@ -10,11 +11,7 @@ function App() {
       <Route path="/login" element={<Login />} />
       <Route
         path="/join"
-        element={
-          <section>
-            <h1>회원가입 페이지</h1>
-          </section>
-        }
+        element={<SignUp />}
       />
     </Routes>
   );
