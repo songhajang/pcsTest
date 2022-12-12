c9eb2bef1ecb35e01921ca3975b7655f3bc22ad2
koldin108902 <alex108902@naver.com>
2022-12-12 18:59:19 +0900

    update: connect to BE api

--- src/component/myPage.jsx
+++ src/component/myPage.jsx
@@ -2,68 +2,112 @@ import { useEffect, useState } from "react";
 import axios from "axios";
 import "./css/myPage.css"
 import Post from "./post"
+import Loading from "./loading"
 import logo from "../img/logo.png";
+const code = process.env.REACT_APP_BACKEND_URL;
 
 export default () => {
-    const data = {
-        postId: 1,
-        description: "test글 작성",
-        created: "2022-12-04T04:53:59.213031",
-        likedCount: 0
+    const [postList, setPostList] = useState()
+    const [postLoading, setPostLoading] = useState(true)
+    const [userInfo, setUserInfo] = useState({
+        name: "-",
+        std_num: "-",
+        birthDay: "-",
+        phoneNumber: "-" 
+    })
+
+    async function getPosts() {
+        try {
+            setPostLoading(true)
+            
+            const {data} = await axios.post(`${code}/member/posts`, {}, {withCredentials: true})
+            const res = data.data
+            
+            setPostList(res.postList)
+        } catch(e) {
+            console.log(e)
+            alert("알 수 없는 오류 발생")
+        } finally {
+            setPostLoading(false)
+        }
     }
+
+    async function myInfo() {
+        try {
+            const {data} = await axios.post(`${code}/member/info`, {}, {withCredentials: true})
+            const res = data.data
+
+            setUserInfo(res)
+        } catch(e) {
+            console.log(e)
+            alert("알 수 없는 오류 발생")
+        }
+    }
+    
+    useEffect(() => {
+        getPosts()
+        myInfo()
+
+        alert("미완성 페이지입니다. \n빠른 시일내에 완성하도록 하겠습니다. 감사합니다.")
+    }, [])
     
     return (
     <div id="myPageMain">
-        <img id="logo" src={logo} />
+        <img id="logo" src={logo} onClick={() => window.location.href="/"} />
         <div id="my-page">
-            <h1 id="myTitle">PCS 마이페이지</h1>
-            <div class="info-container">
-                <div class="user"><span class="user-info">이름 : </span><span class="info name">장송하</span></div>
-                <div class="user"><span class="user-info">학번 : </span><span class="info student-id">30119</span></div>
-                <div class="user"><span class="user-info">생년월일 : </span><span class="info birth">2004.02.07</span></div>
-                <div class="user"><span class="user-info">전화번호 : </span><span class="info phone-number">010-1234-5678</span></div>
+            <h1 id="myTitle" onClick={() => window.location.href="/"}>PCS 마이페이지</h1>
+            <div className="info-container">
+                <div className="user"><span className="user-info">이름 : </span><span className="info name">{userInfo.name}</span></div>
+                <div className="user"><span className="user-info">학번 : </span><span className="info student-id">{userInfo.std_num}</span></div>
+                <div className="user"><span className="user-info">생년월일 : </span><span className="info birth">{userInfo.birthDay}</span></div>
+                <div className="user"><span className="user-info">전화번호 : </span><span className="info phone-number">{userInfo.phoneNumber}</span></div>
 
                 <br />
 
                 {/* <button onClick={}></button> */}
                 {/* TODO section 과 code-box를 선택하면 보이게끔. */}
                 {/* + API연결하기 */}
-                <button class="edit-btn">정보수정</button>
+                {/* <button className="edit-btn">정보수정</button> */}
             </div>
-            <div class="info-container">
+            <div className="info-container">
                 <section
                     className="posts post-box"
                     style={{ justifyContent: "center" }}
                 >
-                    <div id="1" className="post-item">
-                        <Post data={data} code={"https://localhost"} liked={false} reported={false} />
-                    </div>
-                    <div id="1" className="post-item">
-                        <Post data={data} code={"https://localhost"} liked={false} reported={false} />
-                    </div>
+                {postLoading ? (
+                    <Loading />
+                    ) : (
+                    postList.map((data) => {
+                        return (
+                        <div key={data.postId}>
+                            <Post data={data} code={code} liked={data.isLiked} reported={data.isReported} />
+                        </div>
+                        );
+                    })
+                )}
                 </section>
-                <div class="code-box">
-                    <div class="item-container">
-                        <div class="item-view">
-                            <div class="code-item">
-                                <span class="code">1jy5ghs8cow1k</span>
+                {/* <div className="code-box">
+                    <div className="item-container">
+                        <div className="item-view">
+                            <div className="code-item">
+                                <span className="code">1jy5ghs8cow1k</span>
                                 <span>미사용</span>
                             </div>
                         </div>
-                        <div class="item-view">
-                            <div class="code-item">
-                                <span class="code">1jy5ghs8cow1k</span>
+                        <div className="item-view">
+                            <div className="code-item">
+                                <span className="code">1jy5ghs8cow1k</span>
                                 <span>미사용</span>
                             </div>
                         </div>
-                        <div class="item-view">
-                            <div class="code-item">
-                                <span class="code">1jy5ghs8cow1k</span>
+                        <div className="item-view">
+                            <div className="code-item">
+                                <span className="code">1jy5ghs8cow1k</span>
                                 <span>미사용</span>
                             </div>
                         </div>
                     </div>
-                </div>
+                </div> */}
             </div>
         </div>
     </div>
